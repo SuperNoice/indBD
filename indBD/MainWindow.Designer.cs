50126147808c2fddf66616c8cf7a384542797f10
SuperNoice <SuperNoice@yandex.ru>
2020-06-10 14:08:38 +0300

    v0.35 Добавлено меню с базовыми запросами

--- indBD/MainWindow.Designer.cs
+++ indBD/MainWindow.Designer.cs
@@ -28,55 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.mainDataGridView = new System.Windows.Forms.DataGridView();
+            this.dogovorButton = new System.Windows.Forms.Button();
+            this.itemsButton = new System.Windows.Forms.Button();
+            this.salesButton = new System.Windows.Forms.Button();
+            this.categoryButton = new System.Windows.Forms.Button();
+            this.clientsButton = new System.Windows.Forms.Button();
+            this.workersButton = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // mainDataGridView
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.mainDataGridView.AllowUserToAddRows = false;
+            this.mainDataGridView.AllowUserToDeleteRows = false;
+            this.mainDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(525, 470);
-            this.dataGridView1.TabIndex = 0;
+            this.mainDataGridView.BackgroundColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle10.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.mainDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle10;
+            this.mainDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle11.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle11.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle11.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle11.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.mainDataGridView.DefaultCellStyle = dataGridViewCellStyle11;
+            this.mainDataGridView.Location = new System.Drawing.Point(12, 82);
+            this.mainDataGridView.Name = "mainDataGridView";
+            this.mainDataGridView.ReadOnly = true;
+            this.mainDataGridView.RowHeadersVisible = false;
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            this.mainDataGridView.RowsDefaultCellStyle = dataGridViewCellStyle12;
+            this.mainDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.mainDataGridView.Size = new System.Drawing.Size(1118, 502);
+            this.mainDataGridView.TabIndex = 0;
+            // 
+            // dogovorButton
+            // 
+            this.dogovorButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.dogovorButton.Location = new System.Drawing.Point(12, 12);
+            this.dogovorButton.Name = "dogovorButton";
+            this.dogovorButton.Size = new System.Drawing.Size(75, 38);
+            this.dogovorButton.TabIndex = 1;
+            this.dogovorButton.Text = "Договор";
+            this.dogovorButton.UseVisualStyleBackColor = false;
+            this.dogovorButton.Click += new System.EventHandler(this.dogovorButton_Click);
+            // 
+            // itemsButton
+            // 
+            this.itemsButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.itemsButton.Location = new System.Drawing.Point(93, 12);
+            this.itemsButton.Name = "itemsButton";
+            this.itemsButton.Size = new System.Drawing.Size(75, 38);
+            this.itemsButton.TabIndex = 2;
+            this.itemsButton.Text = "Предметы залога";
+            this.itemsButton.UseVisualStyleBackColor = false;
+            this.itemsButton.Click += new System.EventHandler(this.itemsButton_Click);
+            // 
+            // salesButton
+            // 
+            this.salesButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.salesButton.Location = new System.Drawing.Point(174, 12);
+            this.salesButton.Name = "salesButton";
+            this.salesButton.Size = new System.Drawing.Size(75, 38);
+            this.salesButton.TabIndex = 3;
+            this.salesButton.Text = "Продажи";
+            this.salesButton.UseVisualStyleBackColor = false;
+            this.salesButton.Click += new System.EventHandler(this.salesButton_Click);
+            // 
+            // categoryButton
+            // 
+            this.categoryButton.Location = new System.Drawing.Point(255, 12);
+            this.categoryButton.Name = "categoryButton";
+            this.categoryButton.Size = new System.Drawing.Size(75, 38);
+            this.categoryButton.TabIndex = 4;
+            this.categoryButton.Text = "Категории";
+            this.categoryButton.UseVisualStyleBackColor = true;
+            this.categoryButton.Click += new System.EventHandler(this.categoryButton_Click);
+            // 
+            // clientsButton
+            // 
+            this.clientsButton.Location = new System.Drawing.Point(336, 12);
+            this.clientsButton.Name = "clientsButton";
+            this.clientsButton.Size = new System.Drawing.Size(75, 38);
+            this.clientsButton.TabIndex = 5;
+            this.clientsButton.Text = "Клиенты";
+            this.clientsButton.UseVisualStyleBackColor = true;
+            this.clientsButton.Click += new System.EventHandler(this.clientsButton_Click);
+            // 
+            // workersButton
+            // 
+            this.workersButton.Location = new System.Drawing.Point(417, 12);
+            this.workersButton.Name = "workersButton";
+            this.workersButton.Size = new System.Drawing.Size(75, 38);
+            this.workersButton.TabIndex = 6;
+            this.workersButton.Text = "Сотрудники";
+            this.workersButton.UseVisualStyleBackColor = true;
+            this.workersButton.Click += new System.EventHandler(this.workersButton_Click);
             // 
             // button1
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(615, 27);
+            this.button1.Location = new System.Drawing.Point(12, 56);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 22);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Добавить";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 22);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Изменить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 22);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(760, 505);
+            this.ClientSize = new System.Drawing.Size(1142, 596);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.workersButton);
+            this.Controls.Add(this.clientsButton);
+            this.Controls.Add(this.categoryButton);
+            this.Controls.Add(this.salesButton);
+            this.Controls.Add(this.itemsButton);
+            this.Controls.Add(this.dogovorButton);
+            this.Controls.Add(this.mainDataGridView);
+            this.MinimumSize = new System.Drawing.Size(520, 270);
             this.Name = "MainWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "База данных ломбарда";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView mainDataGridView;
+        private System.Windows.Forms.Button dogovorButton;
+        private System.Windows.Forms.Button itemsButton;
+        private System.Windows.Forms.Button salesButton;
+        private System.Windows.Forms.Button categoryButton;
+        private System.Windows.Forms.Button clientsButton;
+        private System.Windows.Forms.Button workersButton;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- indBD/MainWindow.cs
+++ indBD/MainWindow.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -27,13 +28,74 @@ namespace indBD
             if (connectForm.doLogin == false) { this.Close(); this.Dispose(true); }
             connection = connectForm.connection;
 
+            //Инициализация
+            DoSqlCommand(
+                "CREATE DATABASE IF NOT EXISTS ломбард; " +
+                "USE ломбард; " +
 
-        }
+                "CREATE TABLE IF NOT EXISTS сотрудники (" +
+                "`Код сотрудника` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "ФИО VARCHAR(40), " +
+                "Должность VARCHAR(20)" +
+                ")ENGINE = InnoDB; " +
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string sqlCommand = "SHOW DATABASES";
-            dataGridView1.DataSource = DoSqlCommand(sqlCommand);
+                "CREATE TABLE IF NOT EXISTS Категории (" +
+                "`Код категории` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "Категория VARCHAR(40)" +
+                ")ENGINE = InnoDB; " +
+
+                "CREATE TABLE IF NOT EXISTS Клиенты (" +
+                "`Код клиента` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "ФИО VARCHAR(40), " +
+                "`Дата рождения` VARCHAR(10), " +
+                "`Место жительства` VARCHAR(50), " +
+                "Документ VARCHAR(40), " +
+                "`Серия номер паспорта` VARCHAR(15), " +
+                "`Дата выдачи` VARCHAR(10), " +
+                "`Кем выдан` VARCHAR(50), " +
+                "`Контактный телефон` VARCHAR(15)" +
+                ")ENGINE = InnoDB; " +
+
+                "CREATE TABLE IF NOT EXISTS Договор (" +
+                "`Код договора` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "Клиент INT, " +
+                "`Дата выдачи` VARCHAR(10), " +
+                "`Минимальный срок залога` INT, " +
+                "`Максимальный срок залога` INT, " +
+                "`Размер ссуды` INT, " +
+                "`Процент в день` INT, " +
+                "`Процент в день за хранение` INT, " +
+                "`Гарантированный срок хранения` INT, " +
+                "`Фактическая дата выкупа` VARCHAR(10), " +
+                "`Фактическая сумма выкупа` VARCHAR(10), " +
+                "FOREIGN KEY(Клиент) REFERENCES Клиенты(`Код клиента`)" +
+                ")ENGINE = InnoDB; " +
+
+                "CREATE TABLE IF NOT EXISTS `Предметы залога` (" +
+                "`Учетный код` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "`Код категории` INT, " +
+                "Название VARCHAR(30), " +
+                "Количество INT, " +
+                "`Ед изм` VARCHAR(20), " +
+                "Примечание VARCHAR(50), " +
+                "`Оценочная стоимость` INT, " +
+                "`Код товара` INT, " +
+                "Статус VARCHAR(10), " +
+                "FOREIGN KEY(`Код категории`) REFERENCES Категории(`Код категории`), " +
+                "FOREIGN KEY(`Код товара`) REFERENCES Договор(`Код договора`)" +
+                ")ENGINE = InnoDB; " +
+
+                "CREATE TABLE IF NOT EXISTS Продажи (" +
+                "`Код продажи` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "`Учетный код` INT, " +
+                "`Цена продажи` INT, " +
+                "Сотрудник INT, " +
+                "`Дата продажи` VARCHAR(10), " +
+                "FOREIGN KEY(`Учетный код`) REFERENCES `Предметы залога`(`Учетный код`), " +
+                "FOREIGN KEY(Сотрудник) REFERENCES Сотрудники(`Код сотрудника`)" +
+                ")ENGINE = InnoDB; " +
+
+                "");
         }
 
         void ConnectionOpen()
@@ -77,5 +139,65 @@ namespace indBD
             return table;
         }
 
+        private void dogovorButton_Click(object sender, EventArgs e)
+        {
+            setDefaultButtonColor();
+            dogovorButton.BackColor = Color.FromArgb(165, 208, 255);
+            mainDataGridView.DataSource = DoSqlCommand("SELECT * FROM Договор");
+        }
+
+        private void itemsButton_Click(object sender, EventArgs e)
+        {
+            setDefaultButtonColor();
+            itemsButton.BackColor = Color.FromArgb(165, 208, 255);
+            mainDataGridView.DataSource = DoSqlCommand("SELECT * FROM `Предметы залога`");
+        }
+
+        private void salesButton_Click(object sender, EventArgs e)
+        {
+            setDefaultButtonColor();
+            salesButton.BackColor = Color.FromArgb(165, 208, 255);
+            mainDataGridView.DataSource = DoSqlCommand("SELECT * FROM Продажи");
+        }
+
+        private void categoryButton_Click(object sender, EventArgs e)
+        {
+            setDefaultButtonColor();
+            categoryButton.BackColor = Color.FromArgb(165, 208, 255);
+            mainDataGridView.DataSource = DoSqlCommand("SELECT * FROM Категории");
+        }
+
+        private void clientsButton_Click(object sender, EventArgs e)
+        {
+            setDefaultButtonColor();
+            clientsButton.BackColor = Color.FromArgb(165, 208, 255);
+            mainDataGridView.DataSource = DoSqlCommand("SELECT * FROM Клиенты");
+        }
+
+        private void workersButton_Click(object sender, EventArgs e)
+        {
+            setDefaultButtonColor();
+            workersButton.BackColor = Color.FromArgb(165, 208, 255);
+            mainDataGridView.DataSource = DoSqlCommand("SELECT * FROM Сотрудники");
+        }
+
+        private void setDefaultButtonColor(Button button)
+        {
+            button.BackColor = SystemColors.ControlLight;
+        }
+        /// <summary>
+        /// Устанавливает стандартный цвет всем кнопкам меню
+        /// </summary>
+        private void setDefaultButtonColor()
+        {
+            Color color = SystemColors.ControlLight;
+
+            dogovorButton.BackColor = color;
+            itemsButton.BackColor = color;
+            salesButton.BackColor = color;
+            categoryButton.BackColor = color;
+            clientsButton.BackColor = color;
+            workersButton.BackColor = color;
+        }
     }
 }
